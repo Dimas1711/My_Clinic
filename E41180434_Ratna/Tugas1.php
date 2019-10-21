abd972871698a387b3aeaaf76ca9bcbc7003da59
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2019-10-21 23:45:56 +0700

    Create Tugas1.php

---
+++ E41180434_Ratna/Tugas1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>TUGAS 1</title>
+    </head>
+
+    <body>
+        <form method="POST" action="tugas1CRUD.php">
+        NIM : <input type="text" name="nim"><br/>
+        Nama : <input type="text" name="nama"><br/>
+        <input type="submit" name="btn_oke" value="Oke">
+        </form>
+    </body>
+</html>
