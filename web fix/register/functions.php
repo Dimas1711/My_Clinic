ca9287ef9d8929967c8a5d16ce490176ff58c333
Dimas1711 <pdimas429@gmail.com>
2019-11-05 23:18:29 +0700

    Update functions.php

--- web fix/register/functions.php
+++ web fix/register/functions.php
@@ -1,11 +1,28 @@
 <?php
-$conn = mysqli_connect("localhost","root","","coba_klinik_2");
+$conn = mysqli_connect("localhost","root","","coba");
 
 function registrasi($data)
 {
   global $conn;
   
-  $username = ($data["username"]);
-}
+  $id_anggota = $_POST["id_anggota"];
+  $password = $_POST["password"];
+  $ktp = $_POST["ktp"];
+  $nama = $_POST["nama"];
+  $jenis = $_POST["jenis"];
+  $jenis_kelamin = $_POST["jenis_kelamin"];
+  $ttl = $_POST["ttl"];
+  $alamat = $_POST["alamat"];
+  $pendidikan = $_POST["pendidikan"];
+  $nohp = $_POST["nohp"];
+  $pekerjaan = $_POST["pekerjaan"];
+  $email = $_POST["email"];
+  $foto = $_POST["foto"];
+  
+  
+  mysqli_query($conn, "INSERT INTO tb_anggota VALUES
+          ('$id_anggota','$password','$ktp','$nama','$jenis','$jenis_kelamin','$ttl','$alamat','$pendidikan','$nohp','$pekerjaan','$email','$foto')");
 
+          return mysqli_affected_rows($conn);
+}
 ?>
