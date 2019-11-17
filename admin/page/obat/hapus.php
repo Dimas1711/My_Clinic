939a8218f00d8b90e68f350597fc10dace9c6634
Ryan Hartadi <Ryanhartadi06@gmail.com>
2019-11-17 10:47:34 +0700

    Create hapus.php

---
+++ admin/page/obat/hapus.php
@@ -0,0 +1,13 @@
+<?php
+
+$id = $_GET['ID_OBAT'];
+
+$koneksi->query ("delete from tb_obat where ID_OBAT = '$id'");
+
+
+
+?>
+ <script type="text/javascript">
+    
+    window.location.href="?page=obat";
+</script>
