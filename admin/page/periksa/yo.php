146ec7674d7a21c96f15ebd576056b64d85583a5
Ryan Hartadi <Ryanhartadi06@gmail.com>
2019-11-17 10:47:58 +0700

    Create yo.php

---
+++ admin/page/periksa/yo.php
@@ -0,0 +1,18 @@
+<?php
+
+$koneksi = new mysqli("localhost","root","","yo");
+$nama = $_GET['nama'];//sesuai yg ada di
+$sql = mysqli_query($koneksi , "select * from tb_anggota where NAMA_ANGGOTA = '$nama'");
+$anggota = mysqli_fetch_array($sql);
+
+
+
+$data = array (
+  'NAMA_ANGGOTA' => $anggota['NAMA_ANGGOTA'],
+  'ID_ANGGOTA' => $anggota['ID_ANGGOTA'],
+  'NO_KTP_NIM_NIP' => $anggota['NO_KTP_NIM_NIP'],
+  'JENIS_ANGGOTA' => $anggota['JENIS_ANGGOTA']
+);
+echo json_encode($data);
+?>
+ 
