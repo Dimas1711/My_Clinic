7f965c37327e320d5b64c62d52c31c223d3bdd11
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2019-11-29 16:27:05 +0700

    Update input.php
    form rujukan

--- admin/page/periksa/input.php
+++ admin/page/periksa/input.php
@@ -1,1 +1,63 @@
-ini input
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>RUJUKAN</title>
+    
+  <script src="jQuery.js"></script>
+</head>
+<body>
+<div class=" col-sm-12 col-xs-12">                     
+<div class="panel panel-default">
+    <h1>RUJUKAN</h1>
+    <hr>
+      <form method ="POST">
+      <div class="form-group">
+    <label for="ID_RUJUKAN"><b>ID RUJUKAN</b></label>  <input type="text" name="ID_RUJUKAN" disabled>
+    </br>
+      </div>
+      <div class="form-group">
+    <label for="ID_BEROBAT"><b>ID BEROBAT</b></label> <input type="text"   name="ID_BEROBAT" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="ID_ANGGOTA"><b>ID ANGGOTA</b></label> <input type="text" name="ID_ANGGOTA" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="NAMA_ANGGOTA"><b>NAMA ANGGOTA</b></label><input type="text" name="NAMA_ANGGOTA" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="JENIS_KELAMIN"><b>JENIS KELAMIN</b></label><input type="text" name="JENIS_KELAMIN" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="TENSI"><b>TENSI</b></label><input type="text" name="TENSI" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="DIAGNOSA"><b>DIAGNOSA</b></label> <input type="text" name="DIAGNOSA" disabled>    
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="ID_DOKTER"><b>ID DOKTER</b></label> <input type="text"  name="ID_DOKTER" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="NAMA_DOKTER"><b>NAMA DOKTER</b></label> <input type="text" name="NAMA_DOKTER" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="TUJUAN"><b>TUJUAN</b></label> <input type="text" placeholder="Enter TUJUAN"  name="TUJUAN" required>
+    </br>
+    </div>
+    <div class="form-group">
+    <input type="submit" name ="submit" class="printbtn" value="Print"></input>
+    </div>
+</div>
+</div>
+</form> 
+</body>
