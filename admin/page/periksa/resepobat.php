225b7f75d9879ecbd14af24fa41e57bc27eafbf1
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2019-11-29 16:28:13 +0700

    Create resepobat.php
    form resep

---
+++ admin/page/periksa/resepobat.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Resep Obat</title>
+    
+  <script src="jQuery.js"></script>
+</head>
+<body>
+<div class=" col-sm-12 col-xs-12">                     
+<div class="panel panel-default">
+    <h1>Resep Obat</h1>
+    <hr>
+    <form method ="POST">
+    <div class="form-group">
+    <label for="ID_BEROBAT"><b>ID BEROBAT</b></label> <input type="text"   name="ID_BEROBAT" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="ID_ANGGOTA"><b>ID ANGGOTA</b></label> <input type="text" name="ID_ANGGOTA" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="NAMA_ANGGOTA"><b>NAMA ANGGOTA</b></label><input type="text" name="NAMA_ANGGOTA" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="ID_OBAT"><b>ID OBAT</b></label><input type="text" name="ID_OBAT" required>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="NAMA_OBAT"><b>NAMA OBAT</b></label> <input type="text" name="NAMA_OBAT" disabled>    
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="ID_DOKTER"><b>ID DOKTER</b></label> <input type="text"  name="ID_DOKTER" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <label for="NAMA_DOKTER"><b>NAMA DOKTER</b></label> <input type="text" name="NAMA_DOKTER" disabled>
+    </br>
+    </div>
+    <div class="form-group">
+    <input type="submit" name ="submit" class="printbtn" value="Print"></input>
+    </div>
+</div>
+</div>
+</form> 
+</body>
