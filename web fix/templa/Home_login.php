85f239666978201c3942c98d1fbc1b72ea9f53ee
bamke25 <nurullahke25@gmail.com>
2019-12-19 20:51:52 +0700

    Update Home_login.php

--- web fix/templa/Home_login.php
+++ web fix/templa/Home_login.php
@@ -49,7 +49,7 @@ session_start();
 
 
             <img src="LOGO-POLITEKNIK-NEGERI-JEMBER.png" style="float: left; margin: 0px 9px 3px 0px;" width="80" height="80" class="d-inline-block align-top" alt="">
-            <h2><a href="#" style="padding: 35px 0px 0px 90px;">Klinik Pratama Politeknik Negeri Jember</a></h2>
+            <h3><a href="#" style="padding: 35px 0px 0px 90px;">Klinik Pratama Politeknik Negeri Jember</a></h3>
             <!-- <div id="text">
                  Klinik Pratama
                  <br>
