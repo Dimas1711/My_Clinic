3a36315294ef853058dc502266289983d428cd62
bamke25 <nurullahke25@gmail.com>
2019-12-19 11:56:31 +0700

    Update registrasi.php

--- web fix/templa/registrasi.php
+++ web fix/templa/registrasi.php
@@ -48,20 +48,57 @@ else
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="registrasi.css">
+    
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<!-- <noscript> -->
+                <link rel="stylesheet" href="css/registrasi.css"/>
+			<!-- <link rel="stylesheet" href="css/skel-noscript.css" /> -->
+			<link rel="stylesheet" href="css/setel.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		<!-- </noscript> -->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
 </head>
-<body>
-        <header>
-                <img src="logo polije.png" alt="ini gambar">
-                <h1>Klinik Pratama
-                    <br>
-                    Rawat Jalan
-                    <br>
-                    Politeknik Negeri Jember
-                    <br>
-                </h1>
-        </header>
+<body  style="background:#cde1ec;">
+    <div id="header">
+			<div id="logo-wrapper">
+				<div class="container">
+						
+					<!-- Logo -->
+						<div id="logo">
+            <img src="LOGO-POLITEKNIK-NEGERI-JEMBER.png" style="float: left; margin: 0px 9px 3px 0px;" width="80" height="80" class="d-inline-block align-top" alt="">
+            <h1><a href="#" style="padding: 0px 0px 0px 90px;">Klinik Pratama Politeknik Negeri Jember</a></h1>
+            <!-- <div id="text">
+                 Klinik Pratama
+                 <br>
+                 Politeknik Negeri Jember
+            </div> -->
+            </div>
+            
+
+				</div>
+			</div>	
+</div>
+
+			
+	<!-- Banner -->
+  <div id="banner">
+			<div class="container">
+			</div>
+		</div>
+  <!-- /Banner -->
+
         <section>
             <h2> Form Pendaftaran</h2>
             <form action="" method="POST" enctype="multipart/form-data" >
@@ -147,5 +184,8 @@ else
              </table>
              </form>
         </section>
+        <script src="js/jQuery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
