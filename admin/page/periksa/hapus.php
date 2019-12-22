a4287a176123c46996216931cbd5dc23132b6503
Dimas1711 <pdimas429@gmail.com>
2019-12-22 11:37:10 +0700

    cetak resep

--- admin/page/periksa/hapus.php
+++ admin/page/periksa/hapus.php
@@ -3,12 +3,15 @@
 require 'functions_admin.php';
 
 $id = $_GET['ID_DETAIL'];
-$id_berobat = $_GET['ID_BEROBAT'];
+
+// $id_berobat = $_GET['ID_BEROBAT'];
+
+
 if (hapus_resep($id) > 0) 
 {
     echo "<script> 
     alert('Data Berhasil Di Hapus');
-    document.location.href = '?page=periksa&aksi=resepobat&ID_BEROBAT='$id'';
+    document.location.href = '?page=periksa&aksi=resepobat&ID_BEROBAT='$id_berobat''
     </script>";
     
 }
