36c348190dca8fbc6ffafd8046c84c264987366e
Ryan Hartadi <Ryanhartadi06@gmail.com>
2019-12-23 17:07:21 +0700

    update

--- admin/page/verifikasi/verifikasi.php
+++ admin/page/verifikasi/verifikasi.php
@@ -1,7 +1,6 @@
 <?php
 require 'functions_admin.php';
-$anggota = query("select * from tb_anggota WHERE STATUS = 'Pending' ");
-$anggota2 = query("select * from tb_anggota WHERE STATUS = 'Reject' ");
+$anggota = query("select * from tb_anggota  ");
 ?>
 <div class="row">
     <div class="col-md-12">
@@ -62,59 +61,5 @@ $anggota2 = query("select * from tb_anggota WHERE STATUS = 'Reject' ");
                   </div>
 
                   
-                  
-
-                  <div class="row">
-    <div class="col-md-12">
-            <div class="panel-body">
-                <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                        <thead>
-                            <tr>
-                        <th>NO.KTP/NIM/NIP</th>
-                        <th>Nama Anggota</th>
-                        <th>Jenis Anggota</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Tanggal Lahir</th>
-                        <th>Alamat</th>
-                        <th>NO.HP</th>
-                        <th>Pekerjaan/Prodi</th>
-                        <th>Email</th>
-                        <th>Foto</th>
-                        <th>Status</th>
-                        <th>Aksi</th>
-                            </tr>
-                        </thead>
-
-            <?php foreach ($anggota2 as $row ) :  ?>
-                <tr>
-                    
-                    <td><?= $row["NO_KTP_NIM_NIP"];?></td>
-                    <td><?= $row["NAMA_ANGGOTA"];?></td>
-                    <td><?= $row["JENIS_ANGGOTA"];?></td>
-                    <td><?= $row["JENIS_KELAMIN"];?></td>
-                    <td><?= $row["TANGGAL_LAHIR"];?></td>
-                    <td><?= $row["ALAMAT"];?></td>
-                    <td><?= $row["NO_HP"];?></td>
-                    <td><?= $row["PEKERJAAN_PRODI"];?></td>
-                    <td><?= $row["EMAIL"];?></td>
-                    <td><img src="img/<?=  $row["FOTO"]; ?>" alt="" width="50px"></td>
-                    <td><?= $row["STATUS"];?></td>
-                    <td>
-                        <a href="?page=verifikasi&aksi=accept&ID_ANGGOTA=<?= $row["ID_ANGGOTA"];?>" name="hapus" class="btn btn-info">Accept</a>  
-                        <a href="?page=verifikasi&aksi=reject&ID_ANGGOTA=<?= $row["ID_ANGGOTA"]; ?>"onclick="return confirm('Anda Yakin Ingin Reject Data Ini ?');" class="btn btn-danger">Reject</a>
-                    </td>     
-                </tr>
-<?php endforeach; ?>
-                   
-
-
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-
+       
                   
