2485198072871dca0de77f17e7f41f252aeb6a62
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2019-12-25 16:38:42 +0700

    Update laporanberobat.php

--- admin/page/laporan/laporanberobat.php
+++ admin/page/laporan/laporanberobat.php
@@ -7,7 +7,7 @@ require 'functions_admin.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Laporan Rujukan</title>
+  <title>Laporan Berobat</title>
     
   <script src="jQuery.js"></script>
 </head>
@@ -18,7 +18,7 @@ require 'functions_admin.php';
         <!-- Advanced Tables -->
         <div class="panel panel-default">
             <div class="panel-heading">
-            Laporan Rujukan
+            Laporan Berobat
             </div>
             <div class="panel-body">
                 <div class="table-responsive">
@@ -26,6 +26,7 @@ require 'functions_admin.php';
                         <thead>
                             <tr>
                                 <th>Id Berobat</th>
+                                <th>Id Dokter</th>
                                 <th>Id Klinik</th>
                                 <th>Id Anggota</th>
                                 <th>Tensi</th>
@@ -41,7 +42,7 @@ require 'functions_admin.php';
 
                       <?php
                      
-                          $sql = $koneksi -> query ("SELECT ID_BEROBAT , NAMA_KLINIK , NAMA_ANGGOTA , TENSI , TANGGAL_BEROBAT , ANAMNESA , DIAGNOSA , ALERGI_OBAT , CATATAN FROM tb_berobat , tb_anggota , tb_klinik WHERE tb_berobat.ID_KLINIK = tb_klinik.ID_KLINIK AND tb_anggota.ID_ANGGOTA = tb_berobat.ID_ANGGOTA");
+                          $sql = $koneksi -> query ("SELECT tb_berobat.ID_BEROBAT, tb_berobat.ID_KLINIK,tb_berobat.ID_ANGGOTA, tb_berobat.NAMA_ANGGOTA, tb_berobat.ID_DOKTER, tb_berobat.TANGGAL_BEROBAT, tb_berobat.TENSI, tb_berobat.ANAMNESA, tb_berobat.DIAGNOSA, tb_berobat.ALERGI_OBAT, tb_berobat.CATATAN FROM tb_berobat");
            
                           while ($data=$sql ->fetch_assoc()) {
 
@@ -50,8 +51,9 @@ require 'functions_admin.php';
                         
                         
                         <td><?php echo $data ['ID_BEROBAT']; ?></td>
-                        <td><?php echo $data ['NAMA_KLINIK']; ?></td>
-                        <td><?php echo $data ['NAMA_ANGGOTA']; ?></td>
+                        <td><?php echo $data ['ID_DOKTER']; ?></td>
+                        <td><?php echo $data ['ID_KLINIK']; ?></td>
+                        <td><?php echo $data ['ID_ANGGOTA']; ?></td>
                         <td><?php echo $data ['TENSI']; ?></td>
                         <td><?php echo $data ['TANGGAL_BEROBAT']; ?></td>
                         <td><?php echo $data ['ANAMNESA']; ?></td>
