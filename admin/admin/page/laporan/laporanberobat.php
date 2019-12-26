0c84825ba88acac77f157360235bb2dc1631341d
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2019-12-26 22:31:53 +0700

    Update laporanberobat.php

--- admin/admin/page/laporan/laporanberobat.php
+++ admin/admin/page/laporan/laporanberobat.php
@@ -26,11 +26,11 @@ require 'functions_admin.php';
                         <thead>
                             <tr>
                                 <th>Id Berobat</th>
-                                <th>Id Dokter</th>
                                 <th>Id Klinik</th>
                                 <th>Id Anggota</th>
-                                <th>Tensi</th>
+                                <th>Id Dokter</th>
                                 <th>Tanggal Berobat</th>
+                                <th>Tensi</th>
                                 <th>Anamnesa</th>
                                 <th>Diagnosa</th>
                                 <th>Alergi</th>
@@ -42,27 +42,24 @@ require 'functions_admin.php';
 
                       <?php
                      
-                          $sql = $koneksi -> query ("SELECT tb_berobat.ID_BEROBAT, tb_berobat.ID_KLINIK,tb_berobat.ID_ANGGOTA, tb_berobat.NAMA_ANGGOTA, tb_berobat.ID_DOKTER, tb_berobat.TANGGAL_BEROBAT, tb_berobat.TENSI, tb_berobat.ANAMNESA, tb_berobat.DIAGNOSA, tb_berobat.ALERGI_OBAT, tb_berobat.CATATAN FROM tb_berobat");
+                          $sql = $koneksi -> query ("SELECT tb_berobat.ID_BEROBAT, tb_berobat.ID_KLINIK,tb_berobat.ID_ANGGOTA, tb_berobat.ID_DOKTER, tb_berobat.TANGGAL_BEROBAT, tb_berobat.TENSI, tb_berobat.ANAMNESA, tb_berobat.DIAGNOSA, tb_berobat.ALERGI_OBAT, tb_berobat.CATATAN FROM tb_berobat");
            
                           while ($data=$sql ->fetch_assoc()) {
 
                        ?>
                       <tr>
                         
-                        
                         <td><?php echo $data ['ID_BEROBAT']; ?></td>
-                        <td><?php echo $data ['ID_DOKTER']; ?></td>
                         <td><?php echo $data ['ID_KLINIK']; ?></td>
                         <td><?php echo $data ['ID_ANGGOTA']; ?></td>
-                        <td><?php echo $data ['TENSI']; ?></td>
+                        <td><?php echo $data ['ID_DOKTER']; ?></td>
                         <td><?php echo $data ['TANGGAL_BEROBAT']; ?></td>
+                        <td><?php echo $data ['TENSI']; ?></td>
                         <td><?php echo $data ['ANAMNESA']; ?></td>
                         <td><?php echo $data ['DIAGNOSA']; ?></td>
                         <td><?php echo $data ['ALERGI_OBAT']; ?></td>
                         <td><?php echo $data ['CATATAN']; ?></td>
 
-                        
-                        
                       </tr>
 
                       <?php } ?>
