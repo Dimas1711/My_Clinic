a0049fb3e8d22ef0a090d21be02bafcb0c9cf98c
bamke25 <nurullahke25@gmail.com>
2019-12-28 10:40:20 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -88,7 +88,7 @@ $pengumuman = query("SELECT * FROM tb_pengumuman")[0];
                             <a class="dropdown-item" href="struktur_organisasi.php">Struktur Organisasi</a>
                             <a class="dropdown-item" href="civitas.php">Civitas</a>
                             <a class="dropdown-item" href="lain_tes.php">Tata Tertib</a>
-                            <a class="dropdown-item" href="#">Alur Pendaftaran</a>
+                            <a class="dropdown-item" href="Alur_Berobat.php">Alur Berobat</a>
                       </div>
                     </li>
                     <li class="nav-item ">
