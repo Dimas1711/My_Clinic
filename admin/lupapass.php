619b70484876c27102041cf2b62206b5380fcb5e
Dimas1711 <pdimas429@gmail.com>
2019-12-30 20:30:01 +0700

    Update lupapass.php

--- admin/lupapass.php
+++ admin/lupapass.php
@@ -60,7 +60,7 @@ try {
 
     $mail->isHTML(true);                                 
     $mail->Subject = 'Here is the subject';
-    $mail->Body    =  'Silahkan Ganti Password Anda di http://localhost/My_Clinic/admin/user/resetpass.php' ;
+    $mail->Body    =  'Silahkan Ganti Password Anda di http://localhost/My_Clinic/admin/resetpass.php' ;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
