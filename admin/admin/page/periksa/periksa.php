57d599cb573e6563918e4231cc65d3040ee2c00f
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2020-02-02 20:48:02 +0700

    Update periksa.php

--- admin/admin/page/periksa/periksa.php
+++ admin/admin/page/periksa/periksa.php
@@ -169,7 +169,7 @@
                 </div>
               
                 <div class="form-group">
-                    <label>Klinik</label>
+                    <label>Klinik Tujuan</label>
                     <select class="form-control" name="KLINIK">
                     <?php while($data = mysqli_fetch_assoc($result4) ){?>
                     <option name="KLINIK" value="<?php echo $data['ID_KLINIK']; ?>"><?php echo $data['NAMA_KLINIK']; ?>
