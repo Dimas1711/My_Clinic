533e2816d07f1e909134dfd9deac364f4b8aa4f7
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2020-02-02 22:43:58 +0700

    Update resepracikan.php

--- admin/admin/page/periksa/resep/resepracikan.php
+++ admin/admin/page/periksa/resep/resepracikan.php
@@ -194,11 +194,11 @@ if (isset($_POST["biasa"])) {
     <hr>
     <form method ="POST">
     <div class="form-group">
-    <label for=">ID_DETAIL"><b>Id Detail</b></label> <input type="text" name="ID_DETAIL" id="ID_DETAIL" value="<?= $detailnya;?>"  readonly>
+    <label for=">ID_DETAIL"><b>Id Detail</b></label> <input style="margin-left:65px;" type="text" name="ID_DETAIL" id="ID_DETAIL" value="<?= $detailnya;?>"  readonly>
     </br>
     </div>
     <div class="form-group">
-    <label for=">NAMA_OBAT"><b>Nama Obat</b></label> <input type="text" name="NAMA_OBAT" id="NAMA_OBAT"  readonly>
+    <label for=">NAMA_OBAT"><b>Nama Obat</b></label> <input style="margin-left:45px;"type="text" name="NAMA_OBAT" id="NAMA_OBAT"  readonly>
     <input type="hidden" name="ID_OBAT" id="ID_OBAT"  readonly>
     </div>
     <div class="form-group">
@@ -206,11 +206,11 @@ if (isset($_POST["biasa"])) {
   
     </div>
     <div class="form-group">
-    <label for="JUMLAH"><b>Jumlah</b></label><input type="number" name="JUMLAH" id="JUMLAH">
+    <label for="JUMLAH"><b>Jumlah</b></label><input style="margin-left:80px;" type="number" name="JUMLAH" id="JUMLAH">
     </br>
     </div>
     <div class="form-group">
-    <label for="CATATAN"><b>Dosis</b></label><input type="text" name="CATATAN" id="CATATAN" ></textarea>
+    <label for="CATATAN"><b>Dosis</b></label><input style="margin-left:90px;" type="text" name="CATATAN" id="CATATAN" ></textarea>
     </br>
     </div>
 
